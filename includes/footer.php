600a055da587e121488c2f45cc3ecbf17ebdb93f
money1team <115262827+money1team@users.noreply.github.com>
2023-10-16 16:03:47 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Designed By <a href="Group 16" target="_blank">Group 16</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
